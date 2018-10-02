5b9789d9b6a5f472b00df205f8bc3861aae85d37
tyler <tyler.r.hood@gmail.com>
2018-10-02 10:09:57 -0700

    merged darrins keyInputs

--- js/app.js
+++ js/app.js
@@ -2,42 +2,38 @@
 
 var map = [];
 var shadow = [];
-var visibility = 3;
+var visibility = [3, 4, 5];
+var mapSize = [10000, 20000, 30000];
 var COLS = 80;
 var ROWS = 60;
 var canvas = document.getElementById('grid');
 var context = canvas.getContext('2d');
 var busyCoordinates = [];
 var player;
-var isShadowToggled = false;
+var isShadowToggled = true;
 var directions = [-1, 0, 1];
 var errors = 0;
 var maxErrorsCount = 1000;
 var minimumTilesAmount = 1000;
-var grid = document.getElementById('grid');
-document.addEventListener('keypress', keyInput);
-class Player {
-  constructor(level, health, coords, xp) {
-    this.level = level;
-    this.health = health;
-    this.coords = coords;
-    this.xp = xp;
-  }
+document.addEventListener('keydown', keyboardInputHandler, false);
+function Player(userName, coords) {
+  this.userName = userName;
+  this.coords = coords;
 }
 
 startGame();
 
 function startGame() {
-  createMap();
+  createMap(mapSize[0]);
   setTimeout(gameSetUp(), 1000);
   function gameSetUp() {
     generatePlayer();
-    addShadow();
+    addShadow(visibility[0]);
     drawMap(0, 0, COLS, ROWS);
   }
 }
 
-function createMap() {
+function createMap(mapSize) {
   for (var row = 0; row < ROWS; row++) {
     map.push([]);
     for (var col = 0; col < COLS; col++) {
@@ -47,7 +43,7 @@ function createMap() {
   var tiles = 0;
   var x = COLS/2;
   var y = ROWS/2;
-  for (var i = 0; i < 30000; i++) {
+  for (var i = 0; i < mapSize; i++) {
     var increment = directions[Math.floor(Math.random() * directions.length)];
     if (Math.random() < 0.5) {
       x += increment;
@@ -86,11 +82,58 @@ function createMap() {
   }
 }
 
-function addShadow() {
-  var startX = (player.coords.x - visibility) < 0 ? 0 : player.coords.x - visibility;
-  var startY = (player.coords.y - visibility) < 0 ? 0 : player.coords.y - visibility;
-  var endX = (player.coords.x + visibility) >= COLS ? COLS - 1 : player.coords.x + visibility;
-  var endY = (player.coords.y + visibility) >= ROWS ? ROWS - 1 : player.coords.y + visibility;
+function generatePlayer() {
+  var coords = generateValidCoords();
+  player = new Player('Tyler', coords);
+  addObjToMap(player.coords, 2);
+}
+
+function generateValidCoords() {
+  var x = Math.floor(Math.random() * COLS);
+  var y = Math.floor(Math.random() * ROWS);
+  while (!areCoordsFree(x, y)) {
+    x = Math.floor(Math.random() * COLS);
+    y = Math.floor(Math.random() * ROWS);
+  }
+  return {
+    x: x,
+    y: y
+  };
+}
+
+function areCoordsFree(x, y) {
+  if (map[y][x] !== 1) {
+    return false;
+  }
+  for (var i = 0; i < busyCoordinates.length; i++) {
+    try {
+      if (busyCoordinates[i].x === x && busyCoordinates[i].y === y) {
+        return false;
+      }
+    } catch (e) {
+      console.log('Error: ' + e);
+    }
+  }
+  return true;
+}
+
+function addObjToMap(coords, identifier) {
+  map[coords.y][coords.x] = identifier;
+  addBusyCoords(coords.x, coords.y);
+}
+
+function addBusyCoords(x, y) {
+  busyCoordinates.push({
+    x: x,
+    y: y
+  });
+}
+
+function addShadow(difficulty) {
+  var startX = player.coords.x - difficulty;
+  var startY = player.coords.y - difficulty;
+  var endX = player.coords.x + difficulty;
+  var endY = player.coords.y + difficulty;
   for (var row = 0; row < ROWS; row++) {
     shadow.push([]);
     for (var col = 0; col < COLS; col++) {
@@ -118,13 +161,10 @@ function drawMap(startX, startY, endX, endY) {
           color = 'blue';
           break;
         case 3:
-          color = 'red';
+          color = 'purple';
           break;
         case 4:
           color = 'green';
-          break;
-        case 5:
-          color = 'orange';
           break;
         default:
           color = 'grey';
@@ -135,53 +175,6 @@ function drawMap(startX, startY, endX, endY) {
   }
 }
 
-function areCoordsFree(x, y) {
-  if (map[y][x] !== 1) {
-    return false;
-  }
-  for (var i = 0; i < busyCoordinates.length; i++) {
-    try {
-      if (busyCoordinates[i].x === x && busyCoordinates[i].y === y) {
-        return false;
-      }
-    } catch (e) {
-      console.log('Error: ' + e);
-    }
-  }
-  return true;
-}
-
-function addBusyCoords(x, y) {
-  busyCoordinates.push({
-    x: x,
-    y: y
-  });
-}
-
-function generateValidCoords() {
-  var x = Math.floor(Math.random() * COLS);
-  var y = Math.floor(Math.random() * ROWS);
-  while (!areCoordsFree(x, y)) {
-    x = Math.floor(Math.random() * COLS);
-    y = Math.floor(Math.random() * ROWS);
-  }
-  return {
-    x: x,
-    y: y
-  };
-}
-
-function generatePlayer() {
-  var coords = generateValidCoords();
-  player = new Player(1, 100, coords, 30);
-  addObjToMap(player.coords, 2);
-}
-
-function addObjToMap(coords, identifier) {
-  map[coords.y][coords.x] = identifier;
-  addBusyCoords(coords.x, coords.y);
-}
-
 function drawObject(x, y, color) {
   context.beginPath();
   context.rect(x * 10, y * 10, 10, 10);
@@ -189,63 +182,74 @@ function drawObject(x, y, color) {
   context.fill();
 }
 
-function keyInput(e) {
+function keyboardInputHandler(e) {
   e.preventDefault();
   var x = player.coords.x;
   var y = player.coords.y;
   var oldX = player.coords.x;
   var oldY = player.coords.y;
-  switch (e.keyCode) {
-  case 37: //left
+  switch (e.key) {
+  case 'ArrowLeft': //left
+    x--;
+    console.log(`Key pressed e.key`);
+    break;
+  case 'a': //left
     x--;
     break;
-  case 38: // up
+  case 'ArrowUp': // up
     y--;
     break;
-  case 39: // right
+  case 'w': // up
+    y--;
+    break;
+  case 'ArrowRight': // right
     x++;
     break;
-  case 40: // down
+  case 'd': // right
+    x++;
+    break;
+  case 'ArrowDown': // down
+    y++;
+    break;
+  case 's': // down
     y++;
     break;
   default:
     return;
   }
   if(map[y][x] !== 0) {
-    updatePlayerPosition(player.coords.x, player.coords.y, x, y);
-    drawMap(oldX - visibility - 1, oldY - visibility - 1, x + visibility + 2, y + visibility + 2);
+    updatePlayerPosition(player.coords.x, player.coords.y, x, y, visibility[0]);
+    drawMap(oldX - visibility[0] - 1, oldY - visibility[0] - 1, x + visibility[0] + 2, y + visibility[0] + 2);
   }
-};
-
-function removeObjFromMap(x, y) {
-  map[y][x] = 1;
 }
 
-function updatePlayerPosition(oldX, oldY, newX, newY) {
-  removeObjFromMap(oldX, oldY);
+function updatePlayerPosition(oldX, oldY, newX, newY, difficulty) {
+  removeObject(oldX, oldY);
   map[newY][newX] = 2;
   player.coords = {x : newX, y : newY};
-
-  var startX = (oldX - visibility) < 0 ? 0 : oldX - visibility;
-  var startY = (oldY - visibility) < 0 ? 0 : oldY - visibility;
-  var endX = (newX + visibility) >= COLS ? COLS - 1 : newX + visibility;
-  var endY = (newY + visibility) >= ROWS ? ROWS - 1 : newY + visibility;
-
+  var startX = (oldX - difficulty) < 0 ? 0 : oldX - difficulty;
+  var startY = (oldY - difficulty) < 0 ? 0 : oldY - difficulty;
+  var endX = (newX + difficulty) >= COLS ? COLS - 1 : newX + difficulty;
+  var endY = (newY + difficulty) >= ROWS ? ROWS - 1 : newY + difficulty;
   if (oldX > newX) {
-    startX = newX - visibility;
-    endX = oldX + visibility;
+    startX = newX - difficulty;
+    endX = oldX + difficulty;
   }
   if (oldY > newY) {
-    startY = newY - visibility;
-    endY = oldY + visibility;
+    startY = newY - difficulty;
+    endY = oldY + difficulty;
   }
   for (var row = startY; row <= endY; row++) {
     for (var col = startX; col <= endX; col++) {
-      if (row >= newY - visibility && row <= newY + visibility && col >= newX - visibility && col <= newX + visibility) {
+      if (row >= newY - difficulty && row <= newY + difficulty && col >= newX - difficulty && col <= newX + difficulty) {
         shadow[row][col] = 1;
       } else {
         shadow[row][col] = 0;
       }
     }
   }
+}
+
+function removeObject(x, y) {
+  map[y][x] = 1;
 }
